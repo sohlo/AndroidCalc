e5f513fda77bb9e57911c3fdd3f6d4170c10607b
sohlo <sillu.ohlo@gmail.com>
2016-03-06 17:11:10 +0200

    Graafiline osa

---
+++ app/src/main/java/com/ee/calculator/MainActivity.java
@@ -0,0 +1,41 @@
+package com.ee.calculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static com.ee.calculator.R.layout.content_main;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
