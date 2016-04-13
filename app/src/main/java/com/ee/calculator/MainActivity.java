c07d70df282f5a9fbe71a2d1f0bb8f23840a564b
sohlo <sillu.ohlo@gmail.com>
2016-04-13 21:41:43 +0300

    state parandused, ebavajalike kommenteerimiste puhastus

--- app/src/main/java/com/ee/calculator/MainActivity.java
+++ app/src/main/java/com/ee/calculator/MainActivity.java
@@ -14,17 +14,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    /*
-        private static final String STATE_ARRAY = "insertedArray";
-        private static final String STATE_DISPLAY = "insertedArray";
-        private static final String STATE_RESULT = "calulatedResult";
-        private static final String STATE_CURRENT = "currentInsert";
-        private static final String STATE_LAST_INSERTS = "lastInserted";
-        private static final String STATE_SHOWING = "showingResult";
-        private CalculatorEngine calc = new CalculatorEngine();
-    */
-    private static TextView calcText;
-    private static String text;
+    private static final String CURRENT_CALCULATION = "currentCalculation";
+    private static TextView textView;
+    private static String savedCalculation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,60 +25,50 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "onCreate called");
         }
         setContentView(R.layout.activity_main);
-        calcText = (TextView) findViewById(R.id.textViewCalc);
-        calcText.setText(text);
-/*
+        textView = (TextView) findViewById(R.id.textViewCalc);
+
         if (savedInstanceState != null) {
             if (BuildConfig.DEBUG) {
                 Log.d(TAG, "Restoring state");
             }
-            calc.setStringToArray(savedInstanceState.getString(STATE_ARRAY));
-            calc.setCalcResult(savedInstanceState.getDouble(STATE_RESULT));
-            calc.setCurrentNumber(savedInstanceState.getString(STATE_CURRENT));
-            calc.setDisplayText(savedInstanceState.getString(STATE_DISPLAY));
-            calc.setLastInserted(savedInstanceState.getBoolean(STATE_LAST_INSERTS));
-            calc.setShowingResult(savedInstanceState.getBoolean(STATE_SHOWING));
-            calc.setDisplayT();
+            savedCalculation = savedInstanceState.getString(CURRENT_CALCULATION);
+            restoreIntent();
         }
+        textView.setText(savedCalculation);
 
-        currentInput();*/
     }
 
-/*
+    private void restoreIntent() {
+        Intent intent = new Intent();
+        intent.setAction("com.ee.calculatorRequest");
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        intent.putExtra("savedCalculation", savedCalculation);
+        intent.putExtra("restoreState", true);
+
+        sendOrderedBroadcast(intent, null, new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String calcResult = getResultData();
+                textView.setText(calcResult);
+                savedCalculation = calcResult;
+
+            }
+        }, null, Activity.RESULT_OK, null, null);
+
+    }
+
+
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         if (BuildConfig.DEBUG) {
             Log.d(TAG, "onSaveInstanceState called");
         }
-        calc.arrayToString(calc.getInputArray());
-        savedInstanceState.putString(STATE_ARRAY, calc.getStateDisplayTextSave());
-        savedInstanceState.putString(STATE_DISPLAY, calc.getDisplayText());
-        savedInstanceState.putString(STATE_CURRENT, calc.getCurrentNumber());
-        savedInstanceState.putDouble(STATE_RESULT, calc.getCalcResult());
-        savedInstanceState.putBoolean(STATE_LAST_INSERTS, calc.isLastInserted());
-        savedInstanceState.putBoolean(STATE_SHOWING, calc.isShowingResult());
-
+        savedInstanceState.putString(CURRENT_CALCULATION, savedCalculation);
         super.onSaveInstanceState(savedInstanceState);
     }
-*/
-
-
-/*
-    public void currentInput() {
-        calcText.setText(String.format("%s%s", calc.getDisplayText(), calc.getCurrentNumber()));
-
-    }
-*/
 
     public void buttonClicked(View view) {
         broadcastIntent(view);
-
-/*
-        Button btn = (Button) view;
-        String btnIDstring = btn.getResources().getResourceName(btn.getId());
-        calc.checkBtn(btnIDstring);
-        currentInput();
-*/
     }
 
     public void broadcastIntent(View view) {
@@ -96,13 +78,14 @@ public class MainActivity extends AppCompatActivity {
         intent.setAction("com.ee.calculatorRequest");
         intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
         intent.putExtra("insertedButton", btnIDstring);
+        intent.putExtra("restoreState", false);
 
         sendOrderedBroadcast(intent, null, new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 String calcResult = getResultData();
-                calcText.setText(calcResult);
-                text = calcResult;
+                textView.setText(calcResult);
+                savedCalculation = calcResult;
             }
         }, null, Activity.RESULT_OK, null, null);
     }
